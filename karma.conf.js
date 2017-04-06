a3e960a574a3eb3e8ac9703b6c0f041af86127cf
tmartir <informatica_1@espuna.es>
2017-04-06 17:47:14 +0200

    karma sourcemaps working

--- karma.conf.js
+++ karma.conf.js
@@ -2,9 +2,11 @@ module.exports = function(config) {
     config.set({
         frameworks: ["jasmine","source-map-support"],
         files: [
-            { pattern: "dist/**/*test.js" } // *.tsx for React Jsx 
+            { pattern: "dist/**/*test.js" }, // *.tsx for React Jsx 
+            { pattern: "dist/**/*.map",included : false,served:true,watched:false,nocache:true}
         ],
         reporters: ["progress"],
         browsers: ["Chrome"],
+        basePath: "./"
     });
 };
