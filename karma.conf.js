17f9d348a2a2dbc33740ff6f73efb70fcfb7a2d0
tmartir <informatica_1@espuna.es>
2017-04-25 15:44:39 +0200

    Karma_html output

--- karma.conf.js
+++ karma.conf.js
@@ -6,9 +6,25 @@ module.exports = function(config) {
             { pattern: "dist/**/*.map",included : false,served:true,watched:false,nocache:true}
         ],
     
-        reporters: ["progress"],
+        reporters: ["progress","html"],
 
         browsers: ["Chrome"],
-        basePath: "./"
+        
+        basePath: "./",
+            // the default configuration 
+    htmlReporter: {
+      outputDir: 'karma_html', // where to put the reports  
+      templatePath: null, // set if you moved jasmine_template.html 
+      focusOnFailures: true, // reports show failures on start 
+      namedFiles: false, // name files instead of creating sub-directories 
+      pageTitle: null, // page title for reports; browser info by default 
+      urlFriendlyName: false, // simply replaces spaces with _ for files/dirs 
+      reportName: 'report-summary-filename', // report summary filename; browser info by default 
+      
+      
+      // experimental 
+      preserveDescribeNesting: false, // folded suites stay folded  
+      foldAll: false, // reports start folded (only with preserveDescribeNesting) 
+    }
     });
 };
