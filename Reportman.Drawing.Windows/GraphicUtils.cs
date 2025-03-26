77cbc73f1a5fa7a45f0d74e9c3ca8bb32af703f3
PCI5\toni <toni.martir@gmail.com>
2025-03-26 11:41:48 +0100

    Fix namespace

--- Reportman.Drawing.Windows/GraphicUtils.cs
+++ Reportman.Drawing.Windows/GraphicUtils.cs
@@ -9,7 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using static ICSharpCode.SharpZipLib.Zip.ExtendedUnixData;
 
-namespace Reportman.Drawing.Windows
+namespace Reportman.Drawing
 {
     public class GraphicUtils
     {
