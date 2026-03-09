05508f0cf9f8dc70fc3297b2ced44f900a430f65
PCI5\toni <toni.martir@gmail.com>
2026-03-09 15:35:51 +0100

    Resource fix for .net 4.8 framework

--- Reportman.Drawing.Forms/ComboBoxAdvanced.cs
+++ Reportman.Drawing.Forms/ComboBoxAdvanced.cs
@@ -4,7 +4,6 @@ using System.ComponentModel;
 using System.Drawing;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
-
 namespace Reportman.Drawing.Forms
 {
     public delegate void StringEvent(object sender, object nitem);
